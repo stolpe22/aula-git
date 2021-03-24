d0d749d8943d2a17c81278f43bdd896da3f5d2bc
Lucas Stolpe <stolpe02@gmail.com>
2021-03-24 17:27:09 -0400

    Adicionada a mensagem How are you?

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace projeto_teste
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Good Morning!");
+            Console.WriteLine("How are you?");
         }
     }
 }
